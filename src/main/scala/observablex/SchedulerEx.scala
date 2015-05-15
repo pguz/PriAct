b098e230c4c46ffcd0d23c3ed4831311169e5d16
pguz <pawelguz@gmail.com>
2015-05-15 21:02:23 +0200

    GUI

---
+++ src/main/scala/observablex/SchedulerEx.scala
@@ -0,0 +1,11 @@
+package observablex
+
+import rx.lang.scala.Scheduler
+import rx.schedulers.SwingScheduler
+
+object SchedulerEx {
+  
+  val SwingEventThreadScheduler: Scheduler =
+    rx.lang.scala.JavaConversions.javaSchedulerToScalaScheduler(SwingScheduler.getInstance)
+
+}
