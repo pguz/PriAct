0a7e4e21a69d977c54c1bdadb52c6275b0b82150
sopello <sopello@sopello.pl>
2015-05-19 00:27:46 +0200

    AllegroActor: Dodano parsowanie tytułów aukcji oraz linków do opisu. Dodano wczytywanie produktów ze wszystkich stron wyników wyszukiwania.

--- src/main/scala/actor/mining/AllegroActor.scala
+++ src/main/scala/actor/mining/AllegroActor.scala
@@ -1,47 +1,86 @@
 package actor.mining
 
-import akka.actor.{ActorRef}
+import akka.actor.ActorRef
 import org.jsoup.Jsoup
 import org.jsoup.nodes.{Document, Element}
+
 import scala.util.matching.Regex
 
 object AllegroActor  {
   val pricePattern = new Regex("""\d+,\d{2}?""")
   val priceClass = "price"
+  val iterableClass = "excerpt"
 
-  def getSourceCode(product: String): Document = {
+  def getSourceCode(product: String, page: Integer): Document = {
     // Allegro parametry GET'a
     // description=1 -> wyszukuje także w opisach i parametrach
     // p=1 -> numer strony, jesli wyjdziemy wyzej to wyjdzie nam komunikat z pusta strona ("Wygląda na to, że nie mamy tego, czego szukasz.")
     // limit=180 -> limit ilosci wpisow na strone - dostepne 60, 120, 180, inna wartosc wyswietli 60
     // string=asd wyszukiwany tekst
+    // offerTypeBuyNow=1 -> tylko kup teraz
     //TODO: warunek stopu dla pętli pobierania: sprawdzenie czy na stronie wynikowej mamy komunikat zawarty wyżej
-    Jsoup.connect(s"http://allegro.pl/listing/listing.php?bmatch=seng-ps-mp-p-sm-isqm-2-e-0402&order=p&description=1&limit=180&string=$product").get()
+    println("getting product " + product + ", page " + page)
+    Jsoup.connect(s"http://allegro.pl/listing/listing.php?bmatch=seng-ps-mp-p-sm-isqm-2-e-0402&order=p&description=1&limit=60&string=$product&p=$page&offerTypeBuyNow=1").get()
   }
 }
 
 
 class AllegroActor extends CrawlerActor {
   import AllegroActor._
+  import scala.util.control.Breaks._
+  import scala.collection.mutable.ListBuffer
 
   override def getPrices(product: String): List[String]= {
     println("AllegroActor: getPrices")
-    var list: List[String] = List()
+    val contentList: ListBuffer[Document] = ListBuffer()
 
-    //malo funkcyjnie, wykorzystana javowa biblioteka Jsoup
-    val doc = getSourceCode(product)
-    val prices = doc.body().getElementsByClass(priceClass)
-    val price: java.util.Iterator[Element] = prices.iterator()
-    while(price.hasNext()) {
-      val cur_price = price.next()
-      if(cur_price.children().size() > 0) {
-        val ext_price = pricePattern.findFirstIn(cur_price.child(0).text().replaceAll(" ", ""))
-        if(!ext_price.isEmpty) {
-          list = ext_price.get :: list
-        }
+    val list: ListBuffer[String] = ListBuffer()
+
+    var page = 1
+    breakable {
+      while(true) {
+        val pageSource = getSourceCode(product, page)
+        println("got page " + page)
+        if(!hasContentToProcess(pageSource)) break
+        else contentList.append(pageSource)
+        page = page + 1
       }
     }
-    list.reverse.sorted.map(_.replace(',','.'))
+
+    contentList.foreach(processPage(list, _))
+    println("wczytalem produktow: " + list.size + " z rozpoznanych stron: " + contentList.size)
+
+    list.reverse.sorted.map(_.replace(",",".")).toList
+  }
+
+  def hasContentToProcess(preparedDoc: Document): Boolean = {
+    val hasContent = preparedDoc.body().getElementsByClass(iterableClass).size()>0
+    if(hasContent) {
+      println("processing, has content")
+      return true
+    } else {
+      println("not processing, has no content")
+      return false
+    }
+  }
+
+  def processPage(priceList: ListBuffer[String], doc: Document): Unit = {
+    val iteration = doc.body().getElementsByClass(iterableClass)
+    val productList: java.util.Iterator[Element] = iteration.iterator()
+
+    while(productList.hasNext()) {
+      val currentProduct = productList.next()
+      val currentProductPrice = pricePattern.findFirstIn(currentProduct.getElementsByClass(priceClass).text().replaceAll(" ", "")).get
+      val currentProductName = currentProduct.getElementsByClass("details").first().getElementsByTag("h2").text()
+      val currentProductLink = "http://allegro.pl" + currentProduct.getElementsByClass("details").first().getElementsByTag("a").first().attr("href")
+
+      println(currentProductPrice);
+      println(currentProductName)
+      println(currentProductLink)
+      println()
+
+      priceList.append(currentProductPrice)
+    }
   }
 
   override def getDescription(id: String): String = s"Allegro $id: MOCK"
