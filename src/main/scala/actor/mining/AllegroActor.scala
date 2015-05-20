e5dfe283382521b47332cb3b2a8e57438a6b6a6e
sopello <sopello@sopello.pl>
2015-05-20 15:48:40 +0200

    Rozbudowano OlxActor o pobieranie produktów ze wszystkich stron wyszukiwania.

--- src/main/scala/actor/mining/AllegroActor.scala
+++ src/main/scala/actor/mining/AllegroActor.scala
@@ -20,7 +20,7 @@ object AllegroActor  {
     // offerTypeBuyNow=1 -> tylko kup teraz
     //TODO: warunek stopu dla pętli pobierania: sprawdzenie czy na stronie wynikowej mamy komunikat zawarty wyżej
     println("getting product " + product + ", page " + page)
-    Jsoup.connect(s"http://allegro.pl/listing/listing.php?bmatch=seng-ps-mp-p-sm-isqm-2-e-0402&order=p&description=1&limit=60&string=$product&p=$page&offerTypeBuyNow=1").get()
+    Jsoup.connect(s"http://allegro.pl/listing/listing.php?bmatch=seng-ps-mp-p-sm-isqm-2-e-0402&order=p&description=1&limit=180&string=$product&p=$page&offerTypeBuyNow=1").get()
   }
 }
 
--- src/main/scala/actor/mining/OlxActor.scala
+++ src/main/scala/actor/mining/OlxActor.scala
@@ -4,13 +4,18 @@ import akka.actor.{ActorRef}
 import org.jsoup.Jsoup
 import org.jsoup.nodes.{Document, Element}
 
+import scala.collection.mutable.ListBuffer
+import scala.util.control.Breaks._
 import scala.util.matching.Regex
 object OlxActor {
 
   val pricePattern = new Regex("[0-9]+")
+  val urlTrashPattern = "#[^&]+"
   val priceClass = "price"
+  val iterableClass = "offer"
+  val normalOfferTable = "offers_table"
 
-  def getSourceCode(product: String): Document = {
+  def getSourceCode(product: String, page: Integer): Document = {
     // Olx parametry GETa
     // page=1 - numer strony z wynikami, jesli damy za duzy to przekieruje nas do http://olx.pl/oferty/?page=xx czyli wywali
     // nasze zapytanie
@@ -20,7 +25,8 @@ object OlxActor {
     // search[order]=created_at%3Adesc - sortuje od najnowszych
     // view=list -> widok listy; galleryWide - galeria 1; galleryBig - galeria typ 2
     // TODO: warunek stopu dla pętli pobierania - sprawdzenie czy to co dostajemy ma w adresie q-$product
-    Jsoup.connect(s"http://olx.pl/oferty/q-$product/").get()
+    println("getting product " + product + ", page " + page)
+    Jsoup.connect(s"http://olx.pl/oferty/q-$product/?page=$page").get()
   }
 }
 
@@ -29,22 +35,68 @@ class OlxActor extends CrawlerActor {
   import OlxActor._
 
   override def getPrices(product: String): List[(String, String, Double)] = {
-    var list: List[(String, String, Double)] = List()
-    println("OlxActor: getPrices")
-    //malo funkcyjnie, wykorzystana javowa biblioteka Jsoup
-    val doc = getSourceCode(product)
-    val prices = doc.body().getElementsByClass(priceClass)
-    val price: java.util.Iterator[Element] = prices.iterator()
-    while(price.hasNext()) {
-      val cur_price = price.next()
-      if(cur_price.children().size() > 0) {
-        val ext_price = pricePattern.findFirstIn(cur_price.child(0).text().replaceAll(" ", ""))
-        if(!ext_price.isEmpty) {
-          list = ("id", "nazwa", ext_price.get.replace(",",".").toDouble) :: list
+    val contentList: ListBuffer[Document] = ListBuffer()
+
+    val list: ListBuffer[(String, String, Double)] = ListBuffer()
+
+    if(product.length < 0) return list.toList
+
+    var page = 1
+    breakable {
+      while(true) {
+        val pageSource = getSourceCode(product, page)
+        println("got page " + page)
+        if(!hasContentToProcess(pageSource, product)) break
+        else contentList.append(pageSource)
+        page = page + 1
+      }
+    }
+
+    contentList.foldLeft(list)((l, d) => l ++= processPage(d))
+    println("wczytalem produktow: " + list.size + " z rozpoznanych stron: " + contentList.size)
+
+    list.reverse.sorted.toList
+  }
+
+  def hasContentToProcess(preparedDoc: Document, product: String): Boolean = {
+    val docLocation = preparedDoc.location()
+    println("Processing address " + docLocation)
+    if(!(docLocation.contains(product.toLowerCase.replaceAll(" ", "-"))
+      || docLocation.contains(product.toLowerCase.replaceAll(" ", "%20")))) {
+      println("not processing, has q-" + product + " in address")
+      return false
+    }
+
+    val hasContent = preparedDoc.body().getElementsByClass(iterableClass).size()>0
+    if(hasContent) {
+      println("processing, has content")
+      return true
+    } else {
+      println("not processing, has no content")
+      return false
+    }
+  }
+
+  def processPage(doc: Document): ListBuffer[(String, String, Double)] = {
+    // wzięcie ofert tylko z tabeli normalnych ofert, załatwia nam odfiltrowanie powtórzonych ofert wyróżnionych jednocześnie
+    // nie wywalając ofert wyróżnionych
+    val iteration = doc.body().getElementById(normalOfferTable).getElementsByClass(iterableClass)
+    val productList: java.util.Iterator[Element] = iteration.iterator()
+    val pageList = scala.collection.mutable.ListBuffer.empty[(String, String, Double)]
+    while(productList.hasNext()) {
+      val currentProduct = productList.next()
+      if(currentProduct.getElementsByClass(priceClass).size()>0) {
+        breakable {
+          val currentProductPriceOcc = pricePattern.findFirstIn(currentProduct.getElementsByClass(priceClass).text().replaceAll(" ", ""))
+          if(currentProductPriceOcc.size==0) break //konstrukcja continue w przypadku gdy nie mamy ceny a np. "Zamienię" lub "Oddam"
+          val currentProductPrice = currentProductPriceOcc.get
+          val currentProductName = currentProduct.getElementsByTag("h3").first().text()
+          val currentProductLink = currentProduct.getElementsByTag("h3").first().getElementsByTag("a").first().attr("href").replaceAll(urlTrashPattern, "")
+          pageList += ((currentProductLink, currentProductName, currentProductPrice.replace(",",".").toDouble))
         }
       }
     }
-    list.reverse
+    pageList
   }
 
   override def getDescription(id: String): String = s"Olx $id: MOCK"
